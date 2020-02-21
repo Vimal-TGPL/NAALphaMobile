4d2e5a5412c23538e208e330455cdff1cdae82c1
Vimal Kumar <vimal.s@technogradient.com>
2020-02-21 12:23:33 +0530

    Performance

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -39,6 +39,10 @@ const routes: Routes = [
     path: 'change-password',
     loadChildren: () => import('./change-password/change-password.module').then( m => m.ChangePasswordPageModule)
   },
+  {
+    path: 'performance',
+    loadChildren: () => import('./performance/performance.module').then( m => m.PerformancePageModule)
+  },
 ];
 
 @NgModule({
--- src/app/menu/menu-routing.module.ts
+++ src/app/menu/menu-routing.module.ts
@@ -18,6 +18,10 @@ const routes: Routes = [
         path: 'change-password',
         loadChildren: () => import('../change-password/change-password.module').then( m => m.ChangePasswordPageModule)
       },
+      {
+        path: 'performance',
+        loadChildren: () => import('../performance/performance.module').then( m => m.PerformancePageModule)
+      },
     ]
   },
   {
